dfb1a76f523c8fe16a9b0c35ac30bf0c9566ce57
hadi <65118892+hadiazt@users.noreply.github.com>
2022-02-13 13:08:30 +0330

    Better Coding & Added `jalali-moment`

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,25 @@ const { Router } = require("express");
 const router = new Router();
 
 const ytch = require('yt-channel-info')
+const momentJalali = require("jalali-moment");
 
 
 router.get("/", (req, res) => {
     var CHANNEL = req.query.ID
+
+    var INFO = []
+    var VIDEO = []
+    var STATS = []
 
     if (!CHANNEL) {
         res.render("index", {
             icon: "https://www.vectorico.com/wp-content/uploads/2018/02/youtube-icon-300x300.png",
             pageTitle: "YouTube Channel Info",
             CHANNEL,
+            INFO: INFO[0],
             layout: "./"
         });
     } else {
-        var INFO = []
-        var VIDEO = []
-        var STATS = []
 
         const payload = {
             channelId: CHANNEL,
@@ -35,14 +38,17 @@ router.get("/", (req, res) => {
             STATS.push(response)
         })
 
+        
         setTimeout(() => {
+            var TIMEJOIN = momentJalali(STATS[0].joinedDate).locale("fa").format("D MMM YYYY");
             res.render("index", {
                 icon: "https://www.vectorico.com/wp-content/uploads/2018/02/youtube-icon-300x300.png",
                 pageTitle: "YouTube Channel Info",
                 CHANNEL,
-                INFO : INFO[0],
+                INFO: INFO[0],
                 VIDEO: VIDEO[0],
                 STATS: STATS[0],
+                TIMEJOIN,
                 layout: "./"
             });
         }, 12000);
